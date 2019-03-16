fc24f50bf0255261206f36ceffda58dd1550bb3d
czarjulius <juliusczar.jc@gmail.com>
2019-03-16 06:01:26 +0100

    feat(register candidate) added register candidate

--- server/models/candidateQuery.js
+++ server/models/candidateQuery.js
@@ -1,19 +1,23 @@
-const checkUser = 'select * from users where id = $1';
-const checkOffice = 'select * from offices where id = $1';
-const checkParty = 'select * from parties where id = $1';
-const candidateExists = 'select * from candidates where userId = $1 and partyId=$2';
-const insertCandidate = 'INSERT into candidates(officeId, partyId, userId) VALUES($1,$2,$3) RETURNING *';
+const users = 'select * from users where id = $1';
+
+const offices = 'select * from offices where id = $1';
+
+const parties = 'select * from parties where id = $1';
+
+const candidates = 'select * from candidates where userId = $1 and partyId=$2';
+const newCndidate = 'INSERT into candidates(officeId, partyId, userId) VALUES($1,$2,$3) RETURNING *';
+
 const checkCandidate = 'select * from candidates where id = $1';
 const checkVote = 'select * from votes where officeId =$1 and createdBy= $2';
 const insertVote = 'INSERT into votes(createdBy, officeId, candidateId) VALUES($1,$2,$3) RETURNING *';
 const voteResult = 'SELECT officeId, candidateId, COUNT(candidateId) AS result FROM votes WHERE officeId=$1 GROUP BY candidateId, officeId';
 
 export {
-  checkUser,
-  checkOffice,
-  checkParty,
-  candidateExists,
-  insertCandidate,
+  users,
+  offices,
+  parties,
+  candidates,
+  newCndidate,
   checkCandidate,
   checkVote,
   insertVote,
